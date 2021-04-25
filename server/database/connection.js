59e6d6f02ee391789e9abb5e0642a018347e8451
RaghhavDTurki <raghhavdturki@gmail.com>
2021-04-25 18:19:11 +0530

    setting up to push to Heroku

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async() =>{
     try {
         //mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI,{
+        const con = await mongoose.connect("mongodb+srv://admin:raghhavdturki@cluster0.c3tgj.mongodb.net/users?retryWrites=true&w=majority",{
             useNewUrlParser:true,
             useFindAndModify:false,
             useUnifiedTopology:true,
